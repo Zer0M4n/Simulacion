5e2f43088c5610f30872509821a16396f05d4b0f
CESAR GONZALEZ SALAZAR <123420180+Zer0M4n@users.noreply.github.com>
2024-05-24 20:19:44 -0700

    new

---
+++ Libreria/Numeros_Pseudo/Numeros_Pseudo/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros_Pseudo
+{
+    public class Num_Pseudo
+    {
+        private int Constante_Multiplicativa = 0;
+        private int Semilla = 0;
+        private int Constante_Aditiva = 0;
+        private int Modulo = 0;
+        private int N = 0; //Numeros totales que queremos generar
+        private float semilla_auxiliar = 0;
+        private float Auxiliar; //Es el enesimo mas 1 numero 
+
+        //Constructor de la clase
+        public Num_Pseudo(int Constante_Multiplicativa,
+        int Semilla,
+        int Constante_Aditiva,
+        int Modulo,
+        int N)
+        {
+            this.Constante_Multiplicativa = Constante_Multiplicativa;
+            this.Semilla = Semilla;
+            this.Constante_Aditiva = Constante_Aditiva;
+            this.Modulo = Modulo;
+            this.N = N;
+        }
+
+        public List<float> Generador()
+        {
+            semilla_auxiliar = Semilla;
+            List<float> Numeros_pseudoaletorios = new List<float>(N);
+
+            for (int i = 0; i < N; i++)
+            {
+                Auxiliar = (Constante_Multiplicativa * semilla_auxiliar + Constante_Aditiva) % Modulo;
+                semilla_auxiliar = Auxiliar;
+                Numeros_pseudoaletorios.Add(Auxiliar / 100);
+            }
+
+            return Numeros_pseudoaletorios;
+        }
+    }
+}
---
+++ test/test_Num_Pseudo/test_Num_Pseudo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Numeros_Pseudo;
+
+namespace test_Num_Pseudo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int Constante_Multiplicativa = 5;
+            int Semilla = 4;
+            int Constante_Aditiva = 11;
+            int Modulo = 100;
+            int N = 5; //Numeros totales que queremos generar
+            Num_Pseudo rnd = new Num_Pseudo(
+                Constante_Multiplicativa,
+                Semilla,Constante_Aditiva,Modulo,N
+                );
+            List<float> list = new List<float>();
+            list = rnd.Generador();
+
+            Console.WriteLine("Numeros pseudo aletorios");
+            foreach ( float f in list )
+            {
+                Console.WriteLine( f );
+            }
+            Console.ReadKey();
+        }
+    }
+}
